62951619f97e591a8a29e3ddce5c7c26af28ceae
redgato7 <dawidzebacki>
2020-01-12 22:33:17 +0100

    Updates

---
+++ precache-manifest.23bcd475675f97512771a9f3a058a14f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9221f6af88748d7f36f6b52e7c137a50",
+    "url": "/Library-React/index.html"
+  },
+  {
+    "revision": "f3aecef95f06931ffbbb",
+    "url": "/Library-React/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "b42918b4760acc5b7e08",
+    "url": "/Library-React/static/js/2.c9214f43.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/Library-React/static/js/2.c9214f43.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f3aecef95f06931ffbbb",
+    "url": "/Library-React/static/js/main.101e3db3.chunk.js"
+  },
+  {
+    "revision": "80e3769b71e88c28b2a3",
+    "url": "/Library-React/static/js/runtime-main.da2788b7.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Library-React/static/media/logo.5d5d9eef.svg"
+  }
+]);
